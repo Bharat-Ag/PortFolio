8dda3be16406d4f507ba7ccd05c189a7ef075584
Bharat-Ag <108234716+Bharat-Ag@users.noreply.github.com>
2022-12-02 21:45:59 +0530

    added

---
+++ index.js
@@ -0,0 +1,76 @@
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabCtnts = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname) {
+    for (tabLink of tabLinks) {
+        tabLink.classList.remove("active-link");
+    }
+    for (tabCtnt of tabCtnts) {
+        tabCtnt.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// --------------------Navbar animation---------
+let nav = document.querySelector(".navbar-menu");
+
+window.addEventListener('scroll', () => {
+    console.log(scroll);
+
+    if (window.pageYOffset > 250) {
+
+        nav.classList.add("sticky");
+    }
+    else {
+
+        nav.classList.remove("sticky");
+    }
+});
+
+
+// --------------------small screen Navmenu animation---------
+
+let menu = document.querySelector("#side-menu");
+let menuNav = document.querySelector(".navbar-menu")
+
+function openMenu() {
+    menu.classList.toggle("openmenu");
+}
+function closeMenu() {
+    menu.classList.remove("openmenu");
+}
+window.addEventListener('click', (e) => {
+    if (!menuNav.contains(e.target)) {
+        menu.classList.remove("openmenu");
+    }
+})
+
+
+
+
+
+
+
+
+
+// --------------------service section animation---------
+
+
+let svr = document.querySelector(".services-list")
+
+const observer = new IntersectionObserver(
+
+    (entries) => {
+        const ent = entries[0];
+        ent.isIntersecting == false
+            ? svr.classList.remove("card_play")
+            : svr.classList.add("card_play");
+    }, {
+    root: null,
+    rootMargin: "",
+    threshold: 0.2,
+
+});
+
+observer.observe(svr);
